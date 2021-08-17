731cc90ffd617868b2b4ee5e32ceecf526562b28
eavasil <dan_kat@mail.ru>
2021-08-17 20:52:43 +0700

    без аут

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
 public class Main {
-        public static void main(String[] args) throws Exception {
-               System.out.println("Программа расчета бесплатных бонусных миль за перелеты");
-               System.out.println("Введите стоимость вашего билета в рублях: ");
-               Scanner scanner = new Scanner(System.in);
-               int price_fly = (int) scanner.nextDouble();
-               System.out.println("Сумма перевода: " + price_fly);
-               int miles = price_fly / 20;
-               System.out.println("Начисленные мили за перелет: " + miles);
+    public static void main(String[] args) throws Exception {
+        System.out.println("Программа расчета бесплатных бонусных миль за перелеты");
+        System.out.println("Введите стоимость вашего билета в рублях: ");
+        Scanner scanner = new Scanner(System.in);
+        int price_fly = (int) scanner.nextDouble();
+        System.out.println("Стоимость перелета: " + price_fly);
+        int miles = price_fly / 20;
+        System.out.println("Начисленные мили за перелет: " + miles);
 
-        }
     }
+}
